d73431dce89124229c84e78e3356fc0c84e977a1
Mbarek, Mohamed Ali <mbarek@it-2go.de>
2020-07-31 23:14:13 +0200

    web controller updated

--- src/main/java/com/it2go/micro/projectmanagement/web/controller/ProjectController.java
+++ src/main/java/com/it2go/micro/projectmanagement/web/controller/ProjectController.java
@@ -1,5 +1,15 @@
 package com.it2go.micro.projectmanagement.web.controller;
 
+import com.it2go.micro.projectmanagement.services.ProjectService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/projects")
+@RequiredArgsConstructor
 public class ProjectController {
+
+  private final ProjectService projectService;
 
 }
