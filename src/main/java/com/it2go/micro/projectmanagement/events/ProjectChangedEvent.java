7991aad70a1253acbd482100373f67e5227e5599
Mohamed Ali Mbarek <mbarek@it-2go.de>
2021-01-04 15:16:50 +0100

    Refactoring bidirectional relation Project to Employee between two microservices

--- src/main/java/com/it2go/micro/projectmanagement/events/ProjectChangedEvent.java
+++ src/main/java/com/it2go/micro/projectmanagement/events/ProjectChangedEvent.java
@@ -1,10 +1,18 @@
 package com.it2go.micro.projectmanagement.events;
 
 import com.it2go.micro.projectmanagement.domain.Project;
+import java.util.List;
+import java.util.UUID;
+import lombok.Data;
 
+@Data
 public class ProjectChangedEvent extends ProjectEvent {
+
+
+  public List<UUID> removedEmployees;
 
   public ProjectChangedEvent(Project project) {
     super(project);
   }
+
 }
--- src/main/java/com/it2go/micro/projectmanagement/events/ProjectEvent.java
+++ src/main/java/com/it2go/micro/projectmanagement/events/ProjectEvent.java
@@ -2,6 +2,8 @@ package com.it2go.micro.projectmanagement.events;
 
 import com.it2go.micro.projectmanagement.domain.Project;
 import java.io.Serializable;
+import java.util.List;
+import java.util.UUID;
 import lombok.Builder;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
